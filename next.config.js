98ad9a0859fdaeafeb298cf51e6e305fb456d0d7
Anurag Mishra <97835032+shaktimaan00@users.noreply.github.com>
2024-01-24 15:54:09 +0530

    Add NEXT_PUBLIC_ROBOTS_TXT to .env file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
-
+// const withDotenv = require('next-runtime-dotenv');
+// module.exports = withDotenv({
+//   publicRuntimeConfig: {
+//     NEXT_PUBLIC_ROBOTS_TXT: process.env.NEXT_PUBLIC_ROBOTS_TXT,
+//   },
+// });
 module.exports = nextConfig
---
+++ src/app/next-runtime-dotenv.d.ts
@@ -0,0 +1,5 @@
+declare module 'next-runtime-dotenv' {
+    const withDotenv: any;
+    export = withDotenv;
+  }
+  
